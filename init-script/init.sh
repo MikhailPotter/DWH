15a6434047c58787c11f4d90ce9ad79771e553bf
MikhailPotter <mikhail.potter@mail.ru>
2023-11-27 21:16:43 +0300

    Update init.sh

--- init-script/init.sh
+++ init-script/init.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
 set -e
 
 sh /etc/postgresql/init-script/create-replica-user.sh
 sh /etc/postgresql/init-script/backup-master.sh
 sh /etc/postgresql/init-script/init-slave.sh
+cp /etc/postgresql/init-script/config/postgresql.conf /var/lib/postgresql/data
