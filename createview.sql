19ba95e32cbc07a77c00385109aae8fe3b816b28
MikhailPotter <mikhail.potter@mail.ru>
2023-11-27 20:46:09 +0300

    hw3-start

--- createview.sql
+++ createview.sql
@@ -1,14 +1,14 @@
-CREATE VIEW gross_margin_view AS
+CREATE VIEW system.gross_margin_view AS
 SELECT
     s.store_id,
     c.category_id,
     SUM(p.product_price * p.product_count) AS sales_sum
 FROM
-    purchase_items p
+    system.purchase_items p
 JOIN
-    products c ON p.product_id = c.product_id
+    system.products c ON p.product_id = c.product_id
 JOIN
-    purchases s ON p.purchase_id = s.purchase_id
+    system.purchases s ON p.purchase_id = s.purchase_id
 GROUP BY
     s.store_id,
     c.category_id;
